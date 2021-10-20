d806f9b076afbee80f7855d1806985ac0e60eb2b
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2021-10-20 18:47:54 +1300

    Completed Project2

---
+++ project2/truffle-config.js
@@ -0,0 +1,46 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ const infuraKey = "";
+ //
+ // const fs = require('fs');
+ const mnemonic = "either stamp fire bomb require rescue typical embrace dizzy female angle bridge";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.5.1"
+    }
+  }
+};
