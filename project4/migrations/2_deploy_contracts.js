4dfc00362ffd0226e214e65317fcb993a247e0bd
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2022-01-26 18:22:12 +1300

    adding libs

--- project4/migrations/2_deploy_contracts.js
+++ project4/migrations/2_deploy_contracts.js
@@ -1,23 +1,37 @@
-const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+// const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
+const DateLib = artifacts.require("DateLib");
+const RoleLib = artifacts.require("RoleLib");
+const Ownable = artifacts.require("Ownable");
+
 const fs = require('fs');
 
 module.exports = function(deployer) {
+    deployer.deploy(DateLib);
+    deployer.deploy(RoleLib);
+    deployer.deploy(Ownable);
+//    deployer.deploy(FlightSuretyData);
+};  
 
+/*    
     let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    deployer.deploy(FlightSuretyData)
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
-    });
-}
+    deployer.deploy(FlightSuretyData);
+    // .then(() => {
+    //     return deployer.deploy(FlightSuretyApp)
+    //             .then(() => {
+    //                 let config = {
+    //                     localhost: {
+    //                         url: 'http://localhost:8545',
+    //                         dataAddress: FlightSuretyData.address,
+    //                         appAddress: FlightSuretyApp.address
+    //                     }
+    //                 }
+    //                 fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+    //                 fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+    //             });
+    // });
+    deployer.deploy(DateLib);
+    deployer.deploy(RoleLib);
+    deployer.deploy(Ownable);
+};
+*/
--- project4/truffle.js
+++ project4/truffle.js
@@ -1,19 +1,25 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "ce1b404ad7334078bf3d10382c6966ee";
+
+const mnemonic = "raise accuse fluid ceiling pink young heart among quote improve myth point";
 
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
     }
   },
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "^0.8.0"
     }
   }
 };
